e8169d51116dbb3a68e4b5e5fd41e4fba326c7b0
torohangupta <75713762+torohangupta@users.noreply.github.com>
2021-03-29 11:41:36 -0500

    removed weird indentation from main method

--- main_311.m
+++ main_311.m
@@ -20,29 +20,29 @@ clear, clc, close all
 % add dependency filepath
 addpath("./dependencies");
 
-    clear, clc
-    selection = input("What do you want to calculate?\n\n" + ...
-             " 1 | Isentropic Values\n" + ...
-             " 2 | Normal Shock\n" + ...
-             " 3 | Oblique Shock\n\n" + ...
-             " 0 | Exit Program\n\n" + ...
-             "Your choice: ");
-    
-    % function selector
-    clc
-    switch selection
-        case 1
-            fprintf("Isentropic Values Calculator\n\n");
-            isentropic;
-        case 2
-            fprintf("Normal Shock Calculator\n\n");
-            normalShock;
-        case 3
-            fprintf("Oblique Calculator\n\n");
-            obliqueShock;
-        case 0
-            disp("Exiting Program...");
-            selection = 0;
-        otherwise
-            fprintf("That is an invalid selection.");
-    end
+clear, clc
+selection = input("What do you want to calculate?\n\n" + ...
+         " 1 | Isentropic Values\n" + ...
+         " 2 | Normal Shock\n" + ...
+         " 3 | Oblique Shock\n\n" + ...
+         " 0 | Exit Program\n\n" + ...
+         "Your choice: ");
+
+% function selector
+clc
+switch selection
+    case 1
+        fprintf("Isentropic Values Calculator\n\n");
+        isentropic;
+    case 2
+        fprintf("Normal Shock Calculator\n\n");
+        normalShock;
+    case 3
+        fprintf("Oblique Calculator\n\n");
+        obliqueShock;
+    case 0
+        disp("Exiting Program...");
+        selection = 0;
+    otherwise
+        fprintf("That is an invalid selection.");
+end
